d83f80e48241bb79fe528fd15f084fb2e1b9e266
Yigit Sargin <yigitsargin@gmail.com>
2022-11-29 15:56:29 +0300

    test commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import React from 'react';
 const App = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello Worldddd</Text>
+      <Text>Hello Worlddddddd</Text>
     </View>
   );
 };
